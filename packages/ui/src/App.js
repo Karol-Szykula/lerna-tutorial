25862a302ae4bcd9f527e07feec60c27cc3ea75e
karol-szykula <karol.szykula@wp.pl>
2019-01-20 22:26:56 +0100

    Add redux, thunk, material-ui

--- packages/ui/src/App.js
+++ packages/ui/src/App.js
@@ -4,6 +4,24 @@ class App extends Component {
   render() {
     return (
       <div>
+        <div>
+          <button
+          >
+            Sign out
+          </button>
+        </div>
+        <div>
+          <div>
+            <input
+              placeholder="Task description"
+            />
+          </div>
+          <div>
+            <button>
+              Add Task
+          </button>
+          </div>
+        </div>
 
       </div>
     );
