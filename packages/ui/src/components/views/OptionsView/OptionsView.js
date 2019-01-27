5af546b28ea8112109c18fcb2f5b7151561bbf28
karol-szykula <karol.szykula@wp.pl>
2019-01-27 19:28:28 +0100

    Add textField for ip address

--- packages/ui/src/components/views/OptionsView/OptionsView.js
+++ packages/ui/src/components/views/OptionsView/OptionsView.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
+import { TextField } from 'material-ui';
+
+import paperStyle from './../../../styles/paperStyle'
 
 class OptionsView extends React.Component {
 
     render() {
         return (
             <Paper>
-                OptionsView
+                <TextField
+                    style={paperStyle}
+                    name={'Ip_input'}
+                    floatingLabelText={'IP address of the controller.'}
+                />
             </Paper>
         )
     }
---
+++ packages/ui/src/styles/paperStyle.js
@@ -0,0 +1,8 @@
+const paperStyle = {
+    height: 100,
+    margin: 20,
+    textAlign: 'center',
+    display: 'inline-block',
+}
+
+export default paperStyle;
