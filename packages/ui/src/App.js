57af3e45908750efd9f8cb882f13602f88b84ea3
karol-szykula <karol.szykula@wp.pl>
2019-01-27 17:24:41 +0100

    Turn on router

--- packages/ui/src/App.js
+++ packages/ui/src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import ToDo from './ToDo/ToDo'
 
 import Auth from './Auth/Auth';
-import Todo from './ToDo/ToDo';
-// import Router from './Navigation/Router'
+import Router from './Navigation/Router'
 
 class App extends Component {
 
   render() {
     return (
       <Auth >
-        <Todo />
+        <Router />
       </Auth>
     );
   }
